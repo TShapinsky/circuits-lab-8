5459d125423209b8f24b74df810e97532b9f55e8
Adam Selker <adamselker@gmail.com>
2019-04-21 14:24:46 -0400

    better resolution

--- measure-exp3.py
+++ measure-exp3.py
@@ -9,7 +9,7 @@ v = np.linspace(0, 5, 1000)
 
 # vb = 0.559v -> ib = 1μA
 # vb = 1.085 -> ib =  99.8μA
-filename = "data/exp3_direct_vb=1.085.csv"
+filename = "data/exp3_direct_vb=1.085_manual_range.csv"
 measuring = "Vout-Vin"
 
 f = open(filename, 'w')
@@ -18,13 +18,15 @@ f.write("Vout, {!s}\n".format(measuring))
 s.set_voltage(1,0)
 s.autorange(1)
 s.set_voltage(2,3)
-s.set_vrange(2, 0)
+# s.set_vrange(2, 0)
+s.autorange(2)
 
 for val in v:
     s.set_voltage(1,val)
     s.autorange(1)
     s.set_current(2, 0)
-    s.set_vrange(2, 0)
+    s.set_vrange(2, 2)
+    # s.autorange(2)
     f.write('{!s},{!s}\n'.format(val, s.get_voltage(2)))
 
 s.set_current(1,0)
